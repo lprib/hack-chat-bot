3a557ef5b2637a24fb415d1feed733d7fa55cdb1
JaxForReal <jackpribis@gmail.com>
2015-07-25 00:09:41 -0400

    made crypto commands more robust (error handling)

--- source/commands/crypto.js
+++ source/commands/crypto.js
@@ -2,6 +2,10 @@ Cipher = require("simple-cipher");
 
 encrypt = {}
 encrypt.eval = function(data, chatConnection) {
+	if(data.arguments.length < 2){
+		chatConnection.sendMessage("Invalid arguments.");
+		return;
+	}
 	password = data.arguments[0];
 	string = data.argText.substring(password.length + 1, data.argText.length);
 	crypt = Cipher.encrypt(string, password);
@@ -11,7 +15,17 @@ encrypt.help = "Usage: encrypt <password> <text>\n<text> is encrypted with <pass
 
 decrypt = {}
 decrypt.eval = function(data, chatConnection) {
-	crypt = Cipher.decrypt(data.arguments[1], data.arguments[0]);
+	if(data.arguments.length < 2){
+		chatConnection.sendMessage("Invalid arguments.");
+		return;
+	}
+	try{
+		crypt = Cipher.decrypt(data.arguments[1], data.arguments[0]);
+	}catch (exception){
+		chatConnection.sendMessage("Decrypt failed: " + exception.message);
+		return;
+	}
+	
 	chatConnection.sendMessage("Decrypted message: " + crypt);
 }
 decrypt.help = "Usage decrypt <password> <encrypted text>\nDecrypts <encrypted text>."
