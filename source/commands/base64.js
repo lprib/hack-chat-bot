387793b882976029f10bd4018a4a22ad23526ff5
JaxForReal <jackpribis@gmail.com>
2015-07-29 01:25:26 -0700

    added encode and decode for base64

--- source/commands/base64.js
+++ source/commands/base64.js
@@ -1,8 +1,15 @@
-base64 = {}
+base64encode = {}
 
-base64.eval = function(data, chatConnection) {
-	chatConnection.sendMessage(new Buffer(data.argText).toString("base64"));
+base64encode.eval = function(data, chatConnection) {
+	chatConnection.sendMessage("Encoded text: " + new Buffer(data.argText, "utf-8").toString("base64"));
 }
-base64.help = "Usage: base64 <text>\nConverts <text> (as a string) to base64";
+base64encode.help = "Usage: base64encode <text>\nConverts <text> (as a utf-8 string) to base64";
 
-module.exports = {base64: base64};
+base64decode = {}
+
+base64decode.eval = function(data, chatConnection) {
+	chatConnection.sendMessage("Decoded text: " + new Buffer(data.argText, "base64").toString("utf-8"));
+}
+base64decode.help = "Usage: base64decode <text>\nConverts <text> (as a base64 string) to utf-8";
+
+module.exports = {base64encode: base64encode, base64decode: base64decode};
