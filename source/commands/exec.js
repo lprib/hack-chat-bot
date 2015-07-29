c81e052736965314e878c1bf8c58b0addfcb657b
JaxForReal <jackpribis@gmail.com>
2015-07-28 22:05:56 -0700

    added chat confirmation for exec

--- source/commands/exec.js
+++ source/commands/exec.js
@@ -2,6 +2,7 @@ var exec = {};
 
 exec.eval = function(data, chatConnection, commands, config) {
 	console.log(eval(data.argText));
+	chatConnection.sendMessage("Executed.");
 }
 exec.hidden = true;
 
