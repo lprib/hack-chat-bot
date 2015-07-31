58cbbc8bec2810e0beb09946c8ca8f7a467af10f
JaxForReal <jackpribis@gmail.com>
2015-07-31 01:55:00 -0700

    make quote more robust and fix help typo

--- source/commands/quote.js
+++ source/commands/quote.js
@@ -3,8 +3,12 @@ var quote = {};
 quote.eval = function(data, chatConnection, commands, config) {
 	nick = data.arguments[0];
 	userList = config.log[nick];
+	if(userList === undefined) {
+		chatConnection.sendMessage("That user has not chatted since bot initialized.");
+		return;
+	}
 	chatConnection.sendMessage(userList[parseInt(Math.random() * userList.length)] + "\n			-$\\text " + nick + "$");
 }
-quote.help = "Usage: quote <username?\nReturns a random quote by <username>.";
+quote.help = "Usage: quote <username>\nReturns a random quote by <username>.";
 
 module.exports = {quote: quote};
