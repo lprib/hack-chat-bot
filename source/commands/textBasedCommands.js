447c9f06936150ee98f8f92ada24d84265b2b2e6
JaxForReal <jackpribis@gmail.com>
2015-08-02 21:48:26 -0700

    change TextBasedCommands format slightly

--- source/commands/textBasedCommands.js
+++ source/commands/textBasedCommands.js
@@ -3,20 +3,23 @@ var textCommand = function(text) {
     
     this.eval = function(data, chatConnection) {
     parsedText = this.text.replace("%nick%", data.nick);
+    parsedText = this.text.replace("%arg0%", data.arguments[0]);
+
     chatConnection.sendMessage(parsedText);
     }
     this.help = "Takes no arguments.";
 
 }
 
+exp = {};
 
-var hello = new textCommand("Hi there %nick%");
-hello.prototype = textCommand;
+exp.hello = new textCommand("Hi there %nick%");
+exp.hello.prototype = textCommand;
 
-var android = new textCommand("        XXXXXXXX\n      XXXXXXXXXXXX\n     XXX  XXXX  XXX\n    XXXXXXXXXXXXXXXX\n    XXXXXXXXXXXXXXXX\n\n    XXXXXXXXXXXXXXXX\n XX XXXXXXXXXXXXXXXX XX\nXXX XXXXXXXXXXXXXXXX XXX\nXXX XXXXXXXXXXXXXXXX XXX\nXXX XXXXXXXXXXXXXXXX XXX\nXXX XXXXXXXXXXXXXXXX XXX\nXXX XXXXXXXXXXXXXXXX XXX\n XX XXXXXXXXXXXXXXXX XX\n    XXXXXXXXXXXXXXXX\n    XXXXXXXXXXXXXXXX\n      XXX      XXX\n      XXX      XXX\n      XXX      XXX\n       X        X\n\n\"You iOS shills can suck my dick\"\n-Matías Duarte, Android designer");
-android.prototype = textCommand;
+exp.android = new textCommand("        XXXXXXXX\n      XXXXXXXXXXXX\n     XXX  XXXX  XXX\n    XXXXXXXXXXXXXXXX\n    XXXXXXXXXXXXXXXX\n\n    XXXXXXXXXXXXXXXX\n XX XXXXXXXXXXXXXXXX XX\nXXX XXXXXXXXXXXXXXXX XXX\nXXX XXXXXXXXXXXXXXXX XXX\nXXX XXXXXXXXXXXXXXXX XXX\nXXX XXXXXXXXXXXXXXXX XXX\nXXX XXXXXXXXXXXXXXXX XXX\n XX XXXXXXXXXXXXXXXX XX\n    XXXXXXXXXXXXXXXX\n    XXXXXXXXXXXXXXXX\n      XXX      XXX\n      XXX      XXX\n      XXX      XXX\n       X        X\n\n\"You iOS shills can suck my dick\"\n-Matías Duarte, Android designer");
+exp.android.prototype = textCommand;
 
-var cat = new textCommand("hack.cat:\nhttps://twitter.com/Corwinasa/status/624507505637396480\ncredit to @MinusGix");
-cat.prototype = textCommand
+exp.cat = new textCommand("hack.cat:\nhttps://twitter.com/Corwinasa/status/624507505637396480\ncredit to @MinusGix");
+exp.cat.prototype = textCommand;
 
-module.exports = {hello: hello, /*android: android, ios: android,*/ cat: cat};
+module.exports = exp;
