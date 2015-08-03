fb40def487a5afc30447e74b47f7bbf9c226a3c3
JaxForReal <jackpribis@gmail.com>
2015-08-03 01:56:55 -0700

    redid crypto commands...much better

--- source/commands/crypto.js
+++ source/commands/crypto.js
@@ -1,33 +1,44 @@
-Cipher = require("simple-cipher");
+crypto = require("crypto");
 
-encrypt = {}
+var hash = {};
+hash.eval = function(data, chatConnection) {
+	var hashInput = data.argText.substring(data.arguments[0].length + 1, data.argText.length);
+	hashText = crypto.createHash(data.arguments[0]).update(hashInput).digest("hex");
+	chatConnection.sendMessage(data.arguments[0] + " hash: " + hashText);
+}
+hash.help = "Usage: hash <algorithm> <text>\nPerforms a hash algorithm on <text>. Examples of algorithms are md5, sha1, sha256";
+
+var algorithm = "aes-256-ctr";
+var encoding = "base64";
+
+var encrypt = {};
 encrypt.eval = function(data, chatConnection) {
-	if(data.arguments.length < 2){
-		chatConnection.sendMessage("Invalid arguments.");
-		return;
-	}
-	password = data.arguments[0];
-	string = data.argText.substring(password.length + 1, data.argText.length);
-	crypt = Cipher.encrypt(string, password);
-	chatConnection.sendMessage("Password: " + password + "\nText: " + string + "\n" + crypt);
+	text = data.argText.substring(data.arguments[0].length + 1, data.argText.length);
+	cipher = crypto.createCipher(algorithm, data.arguments[0]);
+	result = cipher.update(text, "utf-8", encoding);
+	result += cipher.final(encoding);
+	chatConnection.sendMessage(algorithm + " encrypted text: " + result);
 }
-encrypt.help = "Usage: encrypt <password> <text>\n<text> is encrypted with <password>. Use decrypt to decrypt <text>"
+encrypt.help = "Usage: encrypt <password> <text>\n returns encrypted text";
 
-decrypt = {}
+var decrypt = {};
 decrypt.eval = function(data, chatConnection) {
-	if(data.arguments.length < 2){
-		chatConnection.sendMessage("Invalid arguments.");
-		return;
-	}
-	try{
-		crypt = Cipher.decrypt(data.arguments[1], data.arguments[0]);
-	}catch (exception){
-		chatConnection.sendMessage("Decrypt failed: " + exception.message);
-		return;
-	}
-	
-	chatConnection.sendMessage("Decrypted message: " + crypt);
+	text = data.argText.substring(data.arguments[0].length + 1, data.argText.length);
+	cipher = crypto.createDecipher(algorithm, data.arguments[0]);
+	result = cipher.update(text, encoding, "utf-8");
+	result += cipher.final("utf-8");
+	chatConnection.sendMessage(algorithm + " decrypted text: " + result);
 }
-decrypt.help = "Usage decrypt <password> <encrypted text>\nDecrypts <encrypted text>."
+decrypt.help = "Usage: decrypt <password> <encrypted text>"
 
-module.exports = {encrypt: encrypt, decrypt: decrypt};
+//cryptoConfig algorithm encoding
+//algorithms can be found in ciphers.txt
+var cryptoConfig = {};
+cryptoConfig.eval = function(data, chatConnection) {
+	algorithm = data.arguments[0];
+	encoding = data.arguments[1];
+	chatConnection.sendMessage("Now using algorithm: " + algorithm + ", encoding: " + encoding + " for encrypt and decrypt.");
+}
+cryptoConfig.hidden = true;
+
+module.exports = {hash: hash, encrypt: encrypt, decrypt: decrypt, cryptoConfig: cryptoConfig};
--- source/commands/hash.js
+++
@@ -1,9 +0,0 @@
-md5 = require("md5");
-
-hash = {};
-hash.eval = function(data, chatConnection){
-	chatConnection.sendMessage(md5(data.argText));
-}
-hash.help = "Usage: hash <text>\nReturns the MD5 hash of <text>";
-
-module.exports = {hash: hash};
