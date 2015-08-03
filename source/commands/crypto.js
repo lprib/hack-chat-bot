c91b536db9f8cf2f0c7c36c26249e93a273211cb
JaxForReal <jackpribis@gmail.com>
2015-08-03 02:13:19 -0700

    better encrypt/decrypt with multiple algorithms

--- source/commands/crypto.js
+++ source/commands/crypto.js
@@ -6,39 +6,30 @@ hash.eval = function(data, chatConnection) {
 	hashText = crypto.createHash(data.arguments[0]).update(hashInput).digest("hex");
 	chatConnection.sendMessage(data.arguments[0] + " hash: " + hashText);
 }
-hash.help = "Usage: hash <algorithm> <text>\nPerforms a hash algorithm on <text>. Examples of algorithms are md5, sha1, sha256";
+hash.help = "Usage: hash <algorithm> <text>\nPerforms a hash algorithm on <text>. Examples of algorithms are md5, sha1, sha256\n" + 
+"A list of available hashes can be found here:\nhttps://raw.githubusercontent.com/JaxForReal/hack-chat-bot/master/source/commands/hashes.txt";
 
-var algorithm = "aes-256-ctr";
 var encoding = "base64";
 
 var encrypt = {};
 encrypt.eval = function(data, chatConnection) {
-	text = data.argText.substring(data.arguments[0].length + 1, data.argText.length);
-	cipher = crypto.createCipher(algorithm, data.arguments[0]);
+	text = data.argText.substring(data.arguments[0].length + data.arguments[1].length + 2, data.argText.length);
+	cipher = crypto.createCipher(data.arguments[0], data.arguments[1]);
 	result = cipher.update(text, "utf-8", encoding);
 	result += cipher.final(encoding);
-	chatConnection.sendMessage(algorithm + " encrypted text: " + result);
+	chatConnection.sendMessage(data.arguments[0] + " encrypted text: " + result);
 }
-encrypt.help = "Usage: encrypt <password> <text>\n returns encrypted text";
+encrypt.help = "Usage: encrypt <algorithm> <password> <text>\n returns <text>, encrypted with specified algorithm and password\n" + 
+"A list of algorithms can be found here:\nhttps://raw.githubusercontent.com/JaxForReal/hack-chat-bot/master/source/commands/ciphers.txt";
 
 var decrypt = {};
 decrypt.eval = function(data, chatConnection) {
-	text = data.argText.substring(data.arguments[0].length + 1, data.argText.length);
-	cipher = crypto.createDecipher(algorithm, data.arguments[0]);
+	text = data.argText.substring(data.arguments[0].length + data.arguments[1].length + 2, data.argText.length);
+	cipher = crypto.createDecipher(data.arguments[0], data.arguments[1]);
 	result = cipher.update(text, encoding, "utf-8");
 	result += cipher.final("utf-8");
-	chatConnection.sendMessage(algorithm + " decrypted text: " + result);
+	chatConnection.sendMessage(data.arguments[0] + " decrypted text: " + result);
 }
-decrypt.help = "Usage: decrypt <password> <encrypted text>"
+decrypt.help = "Usage: decrypt <algorithm> <password> <encrypted text>\nDecrypts <algorithm>-encrypted text with <password>"
 
-//cryptoConfig algorithm encoding
-//algorithms can be found in ciphers.txt
-var cryptoConfig = {};
-cryptoConfig.eval = function(data, chatConnection) {
-	algorithm = data.arguments[0];
-	encoding = data.arguments[1];
-	chatConnection.sendMessage("Now using algorithm: " + algorithm + ", encoding: " + encoding + " for encrypt and decrypt.");
-}
-cryptoConfig.hidden = true;
-
-module.exports = {hash: hash, encrypt: encrypt, decrypt: decrypt, cryptoConfig: cryptoConfig};
+module.exports = {hash: hash, encrypt: encrypt, decrypt: decrypt};
